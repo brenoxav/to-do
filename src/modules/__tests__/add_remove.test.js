35107242bf70a30168c7f11a3bc5ffa15f5745bf
tiagorahal <rahal.aires@gmail.com>
2021-07-29 18:40:55 -0300

    Test 'edit' and 'delete all'

--- src/modules/__tests__/add_remove.test.js
+++ src/modules/__tests__/add_remove.test.js
@@ -30,10 +30,42 @@ describe('Add and remove todos', () => {
     expect(todoList.getTodos().length).toBe(1);
   });
 
+  test('Edit an item from the list', () => {
+    const todo = todoList.getTodos()[0];
+
+    editTodo(todo, 'Updated todo');
+
+    const updatedTodo = todoList.getTodos()[0];
+
+    expect(updatedTodo.getDescription()).toBe('Updated todo');
+  });
+
   test('Delete an item from the list', () => {
     const todo = todoList.getTodos()[0];
 
     deleteTodo(todo);
+
+    expect(todoList.getTodos().length).toBe(0);
+  });
+
+  test('Delete all completed todos', () => {
+    const todo1 = new Todo({
+      description: 'Update README',
+      completed: false,
+    });
+
+    const todo2 = new Todo({
+      description: 'Setup linters',
+      completed: false,
+    });
+
+    todoList.addTodo(todo1);
+    todoList.addTodo(todo2);
+
+    todo1.toggleCompleted();
+    todo2.toggleCompleted();
+
+    deleteChecked(todoList);
 
     expect(todoList.getTodos().length).toBe(0);
   });
